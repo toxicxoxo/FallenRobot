ba8a89a293fe33ebf1b61b205c4667c9ea3c2c24
toxicxoxo <106758630+toxicxoxo@users.noreply.github.com>
2022-06-08 00:33:04 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Fallen
-net start Fallen
+net stop FallenRobot
+net start FallenRobot
